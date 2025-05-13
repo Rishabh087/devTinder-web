ee298e1c3d1a0eb1f55af34f91aea777ed86be24
Rishabh087 <rishabhpandey151220012@gmail.com>
2025-05-13 20:59:41 +0530

    navbar-and-routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
     <a className="btn btn-ghost text-xl">💥devTinder</a>
   </div>
   <div className="flex gap-2">
-    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-    <div className="dropdown dropdown-end">
+
+    <div className="dropdown dropdown-end mr-5">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
           <img
