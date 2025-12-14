5b2c7d0ae4df9ae717a9276c4cd7e8050eb938ae
Rishabh087 <rishabhpandey151220012@gmail.com>
2025-12-14 19:09:50 +0530

    fix-1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
  //export const BASE_URL = "http://localhost:3000" ;
- export const BASE_URL = "https://devtinder-web1-ekcm.onrender.com";
+export const BASE_URL = "https://devtinder-1-clbf.onrender.com";
+
 
